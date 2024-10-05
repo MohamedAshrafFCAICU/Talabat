7b84df52389d79954014fde368a0750e38d61022
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-10-05 17:54:50 +0300

    Add project files.

---
+++ LinkDev.Talabat.APIs/Program.cs
@@ -0,0 +1,44 @@
+
+namespace LinkDev.Talabat.APIs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var webApplicationbuilder = WebApplication.CreateBuilder(args);
+
+            #region Configure Services
+            
+            // Add services to the container.
+
+            webApplicationbuilder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            webApplicationbuilder.Services.AddEndpointsApiExplorer();
+            webApplicationbuilder.Services.AddSwaggerGen(); 
+            
+            #endregion
+
+            var app = webApplicationbuilder.Build();
+
+            #region Configure Kestrel Middlewares
+            
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            #endregion
+            
+            app.Run();
+        }
+    }
+}
