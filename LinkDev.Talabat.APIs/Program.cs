3d568d56e979cd69d6e34f6319a9ba66427451c9
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-10-06 15:44:41 +0300

    Apply Migrations (Update-Database)

--- LinkDev.Talabat.APIs/Program.cs
+++ LinkDev.Talabat.APIs/Program.cs
@@ -4,9 +4,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LinkDev.Talabat.APIs
 {
-    public class Program
+    public  class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var webApplicationbuilder = WebApplication.CreateBuilder(args);
 
@@ -27,8 +27,31 @@ namespace LinkDev.Talabat.APIs
 
             var app = webApplicationbuilder.Build();
 
+            #region Update-Database
+          
+            using var scope = app.Services.CreateAsyncScope();
+            var services = scope.ServiceProvider;
+            var dbContext = services.GetRequiredService<StoreContext>(); // Inject StoreContext Explicitly
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>(); // Inject LoggerFactory Explicitly
+
+
+            try
+            {
+                var pendingMigrations = dbContext.Database.GetPendingMigrations();
+                if (pendingMigrations.Any())
+                    await dbContext.Database.MigrateAsync(); // Update - Database
+
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(ex, "An Exception has been occured While Pending The Migrations");
+            } 
+           
+            #endregion
+
             #region Configure Kestrel Middlewares
-            
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
