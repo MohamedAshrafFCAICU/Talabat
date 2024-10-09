be32f5cd95a526842ac44736a9305b2ad0335e1a
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-10-10 02:40:25 +0300

    Product Picture Url Resolver

--- LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
+++ LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
@@ -13,11 +13,15 @@ namespace LinkDev.Talabat.Core.Application.Mapping
 {
     internal class MappingProfile : Profile
     {
+
+      
         public MappingProfile()
         {
             CreateMap<Product, ProductToReturnDto>()
                 .ForMember(d => d.Brand, o => o.MapFrom(src => src.Brand!.Name))
-                .ForMember(d => d.Category, o => o.MapFrom(src => src.Category!.Name));
+                .ForMember(d => d.Category, o => o.MapFrom(src => src.Category!.Name))
+                //.ForMember(d => d.PictureUrl, o => o.MapFrom(src => $"{"http://localhost:5264"}{src.PictureUrl}"));
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductPictureUrlResolver>());
 
             CreateMap<ProductBrand, BrandDto>();
 
