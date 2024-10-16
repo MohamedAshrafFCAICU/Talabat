4c8f44d33e5ba25c50fee8cf78d9c9a5b5631505
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-10-16 04:22:32 +0300

    Basket Service

--- LinkDev.Talabat.Core.Application.Abstraction/Services/IServiceManager.cs
+++ LinkDev.Talabat.Core.Application.Abstraction/Services/IServiceManager.cs
@@ -1,8 +1,10 @@
-﻿using LinkDev.Talabat.Core.Application.Abstraction.Services.Employees;
+﻿using LinkDev.Talabat.Core.Application.Abstraction.Services.Basket;
+using LinkDev.Talabat.Core.Application.Abstraction.Services.Employees;
 using LinkDev.Talabat.Core.Application.Abstraction.Services.Products;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +14,6 @@ namespace LinkDev.Talabat.Core.Application.Abstraction.Services
     {
         public IProductService ProductService { get; }
         public IEmployeeService EmployeeService { get; }
+        public IBasketService BasketService { get;}
     }
 }
--- LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
+++ LinkDev.Talabat.Core.Application/Mapping/MappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using LinkDev.Talabat.Core.Application.Abstraction.Models.Basket;
 using LinkDev.Talabat.Core.Application.Abstraction.Models.Employees;
 using LinkDev.Talabat.Core.Application.Abstraction.Models.Products;
+using LinkDev.Talabat.Core.Domain.Entities.Basket;
 using LinkDev.Talabat.Core.Domain.Entities.Employees;
 using LinkDev.Talabat.Core.Domain.Entities.Products;
 using System;
@@ -28,6 +30,9 @@ namespace LinkDev.Talabat.Core.Application.Mapping
             CreateMap<ProductCategory, CategoryDto>();
 
             CreateMap<Employee, EmployeeToReturnDto>();
+
+            CreateMap<CustomerBasket  , CustomerBasketDto>().ReverseMap();  
+            CreateMap<BasketItem  ,  BasketItemDto>().ReverseMap();  
         }
     }
 }
