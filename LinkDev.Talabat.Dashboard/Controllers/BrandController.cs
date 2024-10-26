aa36d045f8caa9db13322898a60a639a7b5257f0
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-10-27 00:01:31 +0300

    Manage Brands

---
+++ LinkDev.Talabat.Dashboard/Controllers/BrandController.cs
@@ -0,0 +1,38 @@
+﻿using LinkDev.Talabat.Core.Domain.Contracts.Persistance;
+using LinkDev.Talabat.Core.Domain.Entities.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkDev.Talabat.Dashboard.Controllers
+{
+	public class BrandController(IUnitOfWork _unitOfWork) : Controller
+	{
+		public async Task<IActionResult> Index()
+		{
+			var brands = await _unitOfWork.GetRepository<ProductBrand , int>().GetAllAsync();	
+			return View(brands);
+		}
+
+		public async Task<IActionResult> Create(ProductBrand model)
+		{
+			try
+			{
+				await _unitOfWork.GetRepository<ProductBrand, int>().AddAsync(model);
+				await _unitOfWork.CompleteAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			catch (Exception)
+			{
+				ModelState.AddModelError("Name", "Please Enter New Name");
+				return View("Index" ,await _unitOfWork.GetRepository<ProductBrand, int>().GetAllAsync());
+			}
+		}
+
+		public async Task<IActionResult> Delete(int id)
+		{
+			var brand = await _unitOfWork.GetRepository<ProductBrand, int>().GetAsync(id);
+			_unitOfWork.GetRepository<ProductBrand , int>().Delete(brand!);
+			await _unitOfWork.CompleteAsync();
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
