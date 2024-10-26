5fb751687811ebb4a04b5602c282c70ac0246f9a
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-10-26 20:46:35 +0300

    Manage Users

---
+++ LinkDev.Talabat.Dashboard/Models/Users/EditUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LinkDev.Talabat.Dashboard.Models.Roles;
+
+namespace LinkDev.Talabat.Dashboard.Models.Users
+{
+	public class EditUserViewModel
+	{
+		public string Id { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+		public List<EditRoleViewMoel> Roles { get; set; } = null!;
+    }
+}
---
+++ LinkDev.Talabat.Dashboard/Models/Users/ReturnedUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkDev.Talabat.Dashboard.Models.Users
+{
+	public class ReturnedUserViewModel
+	{
+        public string Id { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string DisplayName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string PhoneNumber { get; set; } = null!;
+
+        public IEnumerable<string> Roles { get; set; } = null!;
+
+    }
+}
