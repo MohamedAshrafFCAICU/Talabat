2f9c3e8c3965cc9eba8af9d09a0bca614abf0394
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-10-26 19:20:45 +0300

    Manage Roles

---
+++ LinkDev.Talabat.Dashboard/Models/Roles/CreatedRoleViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDev.Talabat.Dashboard.Models.Roles
+{
+	public class CreatedRoleViewModel
+	{
+		[Required(ErrorMessage = "Name is Required")]
+		[StringLength(256)]
+		public string Name { get; set; } = null!;
+    }
+}
