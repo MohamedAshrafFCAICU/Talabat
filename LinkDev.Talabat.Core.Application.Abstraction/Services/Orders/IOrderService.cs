0d5d2d4aa5a5a0ec597195bdcb90022695e3f223
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-11-23 16:06:51 +0200

    Order Service and Dtos

---
+++ LinkDev.Talabat.Core.Application.Abstraction/Services/Orders/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using LinkDev.Talabat.Core.Application.Abstraction.Models.Orders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.Talabat.Core.Application.Abstraction.Services.Orders
+{
+    public interface IOrderService
+    {
+        Task<OrderToReturnDto> CreateOrderAsync(string buyerEmail , OrderToCreateDto order);
+
+        Task<OrderToReturnDto> CreateOrderByIdAsync(string buyerEmail , int orderId);
+
+        Task<IEnumerable<OrderToReturnDto>> GetOrdersForUserAsync(string buyerEmail);
+        Task<IEnumerable<DeliveryMethodDto>> GetDeliveryMethodAsync();
+    }
+}
