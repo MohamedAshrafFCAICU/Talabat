f775acb6a8614bc2b98d8ec5d5100adbf89633bb
Mohamed Ashraf <mohamedashraf35000@gmail.com>
2024-11-24 17:31:24 +0200

    GetOrdersForUser, GetOrderForUser, GetDeliveryMethods endpoints

--- LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrdersController.cs
+++ LinkDev.Talabat.APIs.Controllers/Controllers/Orders/OrdersController.cs
@@ -26,5 +26,39 @@ namespace LinkDev.Talabat.APIs.Controllers.Controllers.Orders
 
             return Ok(result);
         }
+
+        [HttpGet] //GET: /api/Orders
+        public async Task<ActionResult<IEnumerable<OrderToReturnDto>>> GetOrdersForUser()
+        {
+            //var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            var buyerEmail = "ahmed.nasr@linkdev.com"; // To Be Changed After Modifing The Interceptor
+
+            var result = await serviceManager.OrderService.GetOrdersForUserAsync(buyerEmail);
+                    
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")] //GET: /api/Orders/{id}
+        public async Task<ActionResult<OrderToReturnDto>> GetOrder(int id)
+        {
+            //var buyerEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            var buyerEmail = "ahmed.nasr@linkdev.com"; // To Be Changed After Modifing The Interceptor
+
+            var result = await serviceManager.OrderService.CreateOrderByIdAsync( buyerEmail, id);
+
+            return Ok(result);
+        }
+
+        [HttpGet("deliveryMethods")] //GET: /api/Orders/deliveryMethods
+        public async Task<ActionResult<IEnumerable<DeliveryMethodDto>>> GetDeliveryMethods()
+        {
+          
+            var result = await serviceManager.OrderService.GetDeliveryMethodAsync();
+
+            return Ok(result);
+        }
+
     }
 }
